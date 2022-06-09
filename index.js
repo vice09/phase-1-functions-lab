722d9d36e70de5b3f6b1b14afb179b9eec5656bb
Edwin Victor Oduor Odhiambo <oduoredwin43@gmail.com>
2022-06-09 15:24:28 +0300

    commit tree 20c4918511884218160992f2effe86d25deba0a4

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+function distanceFromHqInBlocks(blocksNumber) {
+    if (blocksNumber > 42) {
+        return "blockNumber <42 === 1"
+     } else {
+         return "blockNumber <42 === 8";
+     }
+    }
+    function distanceFromHqInFeet(blockNumber){
+        return distanceFromHqInBlocks(blockNumber)* 264
+    }
+let start=start
+let destination=destination
+
+function distanceTravelledInFeet(start,destination) {
+    if (start < destination) {
+        return (destination-start)*264
+     }  else {
+         return (start-destination)*264
+
+     }
+    }
+    function calculatesFarePrice(start, destination){
+        var feet = distanceTravelledInFeet(start, destination);
+        if(feet <= 400){
+            return 0
+        }
+        else if ( feet >400 && feet <2000){
+            return 0.02 *(feet -400)
+        }
+        else if (feet >2000 && feet <2500){
+            return 25
+        }
+        else{
+            return "cannot travel that far"
+        }
+    }
+   
+
+
+    
+    
+
+    
 // Code your solution in this file!
